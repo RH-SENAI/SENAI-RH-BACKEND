393e60e1a4799c9be8fabffc66cf06e7d3961fbb
Enzzo Jacó <enzzo.jaco@portalsesisp.org.br>
2022-03-31 16:45:53 -0200

    feat: Adiciona Api

---
+++ GP2/API_G2/SenaiRH_G2/SenaiRH_G2/Domains/Curso.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SenaiRH_G2.Domains
+{
+    public partial class Curso
+    {
+        public int IdCurso { get; set; }
+        public int IdEmpresa { get; set; }
+        public string NomeCurso { get; set; }
+        public string DescricaoCurso { get; set; }
+        public string SiteCurso { get; set; }
+        public bool ModalidadeCurso { get; set; }
+        public string CaminhoImagemCurso { get; set; }
+        public int CargaHoraria { get; set; }
+        public DateTime DataFinalizacao { get; set; }
+        public decimal MediaAvaliacaoCurso { get; set; }
+
+        public virtual Empresa IdEmpresaNavigation { get; set; }
+    }
+}
---
+++ GP2/API_G2/SenaiRH_G2/SenaiRH_G2/Domains/Usuario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SenaiRH_G2.Domains
+{
+    public partial class Usuario
+    {
+        public Usuario()
+        {
+            AvaliacaousuarioIdUsuarioAvaliadoNavigations = new HashSet<Avaliacaousuario>();
+            AvaliacaousuarioIdUsuarioAvaliadorNavigations = new HashSet<Avaliacaousuario>();
+            Decisaos = new HashSet<Decisao>();
+            Feedbacks = new HashSet<Feedback>();
+            Minhasatividades = new HashSet<Minhasatividade>();
+        }
+
+        public int IdUsuario { get; set; }
+        public byte IdCargo { get; set; }
+        public int IdUnidadeSenai { get; set; }
+        public byte IdTipoUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public short Vantagens { get; set; }
+        public decimal NivelSatisfacao { get; set; }
+        public string Cpf { get; set; }
+        public int SaldoMoeda { get; set; }
+        public int Trofeus { get; set; }
+        public string LocalizacaoUsuario { get; set; }
+        public string CaminhoFotoPerfil { get; set; }
+        public decimal Salario { get; set; }
+
+        public virtual Cargo IdCargoNavigation { get; set; }
+        public virtual Tipousuario IdTipoUsuarioNavigation { get; set; }
+        public virtual Unidadesenai IdUnidadeSenaiNavigation { get; set; }
+        public virtual ICollection<Avaliacaousuario> AvaliacaousuarioIdUsuarioAvaliadoNavigations { get; set; }
+        public virtual ICollection<Avaliacaousuario> AvaliacaousuarioIdUsuarioAvaliadorNavigations { get; set; }
+        public virtual ICollection<Decisao> Decisaos { get; set; }
+        public virtual ICollection<Feedback> Feedbacks { get; set; }
+        public virtual ICollection<Minhasatividade> Minhasatividades { get; set; }
+    }
+}
