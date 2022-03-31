3b7bd3c3efa3228efe5d27a265dea9e5b6006292
Enzzo Jacó <enzzo.jaco@portalsesisp.org.br>
2022-03-31 18:13:35 -0200

    feat: Adicionei Interface Curso

---
+++ GP2/API_G2/SenaiRH_G2/SenaiRH_G2/Interfaces/ICursoRepository.cs
@@ -0,0 +1,22 @@
+﻿using SenaiRH_G2.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiRH_G2.Interfaces
+{
+    interface ICursoRepository
+    {
+
+        List<Curso> ListarTodos();
+        List<Curso> ListarCursoFavorito(int id);
+        List<Curso> ListarComentarioCurso(int id);
+        List<Curso> ListarRegistroCurso(int id);
+        void CadastrarCurso(Curso novoCurso);
+        void ExcluirCurso(int id);
+        Curso BuscarPorId(int id);
+
+
+    }
+}
