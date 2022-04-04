81530fbc7f674b103810a38106ae5de900fb6991
MurilloAssis <murillo98asiss@gmail.com>
2022-04-04 16:00:19 -0300

    API GP1 Adicionada

---
+++ Controllers/AtividadesController.cs
@@ -0,0 +1,315 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SenaiRH_G1.Contexts;
+using SenaiRH_G1.Domains;
+using SenaiRH_G1.Interfaces;
+using SenaiRH_G1.Repositories;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SenaiRH_G1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AtividadesController : ControllerBase
+    {
+        private readonly senaiRhContext _context;
+        private readonly IAtividadeRepository _atividadeRepository;
+        public AtividadesController(senaiRhContext context, IAtividadeRepository repo)
+        {
+            _context = context;
+            _atividadeRepository = repo;
+        }
+
+        
+        [Authorize(Roles = "2")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Atividade>>> GetAtividades()
+        {
+            return await _context.Atividades.ToListAsync();
+        }
+
+        [Authorize(Roles = "2")]
+        [HttpPost]
+        public IActionResult PostAtividade(Atividade atividade)
+        {
+            try
+            {
+
+                if (atividade.NomeAtividade == null)
+                {
+                    return BadRequest(new
+                    {
+                        Mensagem = "Os dados estão incorretos"
+                    });
+                }
+                atividade.DataCriacao = DateTime.Now;
+                _context.Atividades.Add(atividade);
+                _context.SaveChanges();
+
+                return StatusCode(201, new
+                {
+                    Mensagem = "Atividade cadastrada",
+                    atividade
+                });
+            }
+            catch (Exception error)
+            {
+
+                return BadRequest(error.Message);
+            }
+
+        }
+
+        [Authorize(Roles ="2")] 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEquipamento(int id)
+        {
+            var atividade = await _context.Atividades.FindAsync(id);
+            if (atividade == null)
+            {
+                return NotFound();
+            }
+
+            _context.Atividades.Remove(atividade);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Endpoint de listar atividades do usuário
+        /// </summary>
+        /// <param name="id">ID do usuário que terá suas atividades listadas</param>
+        /// <returns>Lista de atividades</returns>
+        [Authorize]
+        [HttpGet("MinhasAtividade/{id}")]
+        public IActionResult ListarMinhasAtividades(int id)
+        {
+            try
+            {
+                //Busca usuário pelo ID fornecido
+                Usuario usuario = _context.Usuarios.FirstOrDefault(u => u.IdUsuario == id);
+
+                //Verifica se o usuário é válido
+                if (usuario != null)
+                {              
+                    //Caso seja, retorna status code 200 com a lista de atividade.
+                    return Ok(_atividadeRepository.ListarMinhas(id));
+                }
+                //Caso não seja
+                else
+                {
+                    //Retorna status code 400 com mensagem de erro
+                    return BadRequest(new
+                    {
+                        Mensagem = "O ID inserido é inválido!"
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);  
+            }
+        }
+
+        /// <summary>
+        /// Endpoint de associar um usuário à uma atividade
+        /// </summary>
+        /// <param name="idUsuario">ID do usuário que será associado</param>
+        /// <param name="idAtividade">ID da atividade que será associada</param>
+        /// <returns>Mensagem de confirmação</returns>
+        [Authorize]
+        [HttpPost("Associar/{idUsuario}")]
+        public IActionResult AssociarAtividade(int idUsuario, int idAtividade)
+        {
+            try
+            {
+                //Busca atividade pelo ID fornecido.
+                Atividade atividade = _context.Atividades.FirstOrDefault(a => a.IdAtividade == idAtividade);
+
+                //Busca usuário fornecido
+                Usuario usuario = _context.Usuarios.FirstOrDefault(u => u.IdUsuario == idUsuario);
+
+                //Verifica se atividade e usuário fornecidos são válidos
+                if (usuario != null && atividade != null)
+                {
+                    //Caso válido, chama método de AssociarAtividade
+                    _atividadeRepository.AssociarAtividade(idUsuario, idAtividade);
+
+                    //Ao finalizar, retorna status code 200 com mensagem de sucesso.
+                    return Ok(new
+                    {
+                        Mensagem = "O usuário foi associado a atividade!"
+                    });
+                }
+                //Caso não seja válido
+                else
+                {
+                    //Retorna status code 400 com mensagem de erro
+                    return BadRequest(new
+                    {
+                        Mensagem = "O ID usuário ou o ID Atividade são inválidos;"
+                    });
+                };
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Endpoint de finalizar uma atividade que está em produção
+        /// </summary>
+        /// <param name="idAtividade">ID da atividade que será finalizada</param>
+        /// <returns>Mensagem de confirmação</returns>
+        [Authorize]
+        [HttpPatch("FinalizarAtividade/{idAtividade}")]
+        public IActionResult FinalizarAtividade(int idAtividade)
+        {
+            try
+            {
+                //Busca o ID do usuário logado
+                int idUsuario = Convert.ToInt32(HttpContext.User.Claims.First(c => c.Type == JwtRegisteredClaimNames.Jti).Value);
+
+                //Busca atividade pelo ID fornecido
+                Atividade atividade = _context.Atividades.FirstOrDefault(a => a.IdAtividade == idAtividade);
+
+                //Busca usuário pelo ID do usuário logado
+                Usuario usuario = _context.Usuarios.FirstOrDefault(u => u.IdUsuario == idUsuario);
+
+                //Busca associação entre o usuário e a atividade
+                Minhasatividade minhaAtividade = _context.Minhasatividades.FirstOrDefault(a => a.IdAtividade == idAtividade && a.IdUsuario == idUsuario);
+
+                //Verifica se atividade e usuário fornecidos são válidos
+                if (atividade != null && usuario != null)
+                {
+                    //Caso sejam válidos, verifica se associação entre funcionário e atividade é existente
+                    if (minhaAtividade != null)
+                    {
+                        //Caso seja existente, chama o método de Finalizar a atividade
+                        _atividadeRepository.FinalizarAtividade(idUsuario, idAtividade);
+
+                        //Verifica se a atividade tem necessidade de validação
+                        if (atividade.NecessarioValidar)
+                        {
+                            //Caso a atividade tenha necessidade de validar, a atividade terá seu status alterado e retornará status code 200 com mensagem com essa informação.
+                            return Ok(new
+                            {
+                                Mensagem = "Sua atividade está aguardando confirmação do administrador!"
+                            });
+                        }
+                        //Caso não tenha necessidade de validação
+                        else
+                        {
+                            //Retornará status code 200 com uma mensagem com a confirmação da conclusão da atividade
+                            return Ok(new
+                            {
+                                Mensagem = "Sua atividade foi finalizada com sucesso!"
+                            });
+                        }
+                    }
+                    //Caso não seja existente
+                    else
+                    {
+                        //Retorn status code 400 com mensagem de erro
+                        return BadRequest(new
+                        {
+                            Mensagem = "O usuário não está associado a atividade"
+                        });
+                    }
+
+                }
+                //Caso não sejam válidos
+                else
+                {
+                    //Retorna status code 400 com mensagem de erro.
+                    return BadRequest(new
+                    {
+                        Mensagem = "Os ID's informados são inválidos!"
+                    });
+
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Endpoint de validar uma atividade que foi finalizada pelo usuário, porém, precisa de validação.
+        /// </summary>
+        /// <param name="idAtividade">ID da atividae que será validada.</param>
+        /// <param name="idUsuario">ID do usuario que terá sua atividade validada.</param>
+        /// <returns>Mensagem de confirmação</returns>
+        [Authorize(Roles = "2")]
+        [HttpPatch("ValidarAtividade/{idAtividade}/{idUsuario}")]
+        public IActionResult ValidarAtividade(int idAtividade, int idUsuario)
+        {
+            try
+            {
+                //Busca associação entre a atividade e o usuário
+                Minhasatividade minhaAtividade = _context.Minhasatividades.FirstOrDefault(a => a.IdAtividade == idAtividade && a.IdUsuario == idUsuario);
+
+                // Busca atividade pelo ID fornecido
+                Atividade atividade = _context.Atividades.FirstOrDefault(a => a.IdAtividade == idAtividade);
+
+                //Busca usuário pelo ID fornecido
+                Usuario usuario = _context.Usuarios.FirstOrDefault(u => u.IdUsuario == idUsuario);
+
+                //Verifica se atividade e usuário fornecidos são válidos
+                if(atividade != null && usuario != null)
+                {
+                    //Caso sejam, verifica se associação entre funcionário e atividade é existente
+                    if (minhaAtividade != null)
+                    {
+                        //Caso seja, chamará o método que valida atividade
+                        _atividadeRepository.ValidarAtividade(idAtividade, idUsuario);
+
+                        //Ao finalizar, retornará status code 200 com mensagem de sucesso.
+                        return Ok(new
+                        {
+                            Mensagem = "A atividade foi validada!"
+                        });
+                    }
+                    //Caso não seja
+                    else
+                    {
+                        //Retorna status code 400 com mensagem de erro.
+                        return BadRequest(new
+                        {
+                            Mensagem = "O usuário não está associado a essa atividade!"
+                        });
+                    }
+                }
+                //Caso não sejam
+                else
+                {
+                    //Retornam status code 400 com mensagem de erro.
+                    return BadRequest(new
+                    {
+                        Mensagem = "Os ID's inseridos são inválidos!"
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ Repositories/AtividadeRepository.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using SenaiRH_G1.Contexts;
+using SenaiRH_G1.Domains;
+using SenaiRH_G1.Interfaces;
+using SenaiRH_G1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SenaiRH_G1.Repositories
+{
+    public class AtividadeRepository : IAtividadeRepository
+    {
+        private readonly senaiRhContext ctx;
+        public AtividadeRepository(senaiRhContext appContext)
+        {
+            ctx = appContext;
+        }
+
+        /// <summary>
+        /// Método para associar um usuário à uma atividade
+        /// </summary>
+        /// <param name="idUsuario">ID do usuário que será associado à atividade</param>
+        /// <param name="idAtividade">ID da atividade a qual o usuário será associado</param>
+        public void AssociarAtividade(int idUsuario, int idAtividade)
+        {
+            //Criando nova associação de usuáro com atividade
+            Minhasatividade novaAssociacao = new Minhasatividade();
+
+            //Buscando as entidades por meio dos ID's fornecidos
+            Usuario usuario = ctx.Usuarios.FirstOrDefault(u => u.IdUsuario == idUsuario);
+            Atividade atividade = ctx.Atividades.FirstOrDefault(u => u.IdAtividade == idAtividade);
+            Cargo cargo = ctx.Cargos.FirstOrDefault(u => u.IdCargo == usuario.IdCargo);
+
+            //Verifica se a atividade e se o usuário fornecidos são validos 
+            if (atividade != null && usuario != null)
+            {
+                //Atribui os dados à nova associação
+                novaAssociacao.IdUsuario = idUsuario;
+                novaAssociacao.IdAtividade = idAtividade;
+                novaAssociacao.IdSetor = cargo.IdSetor;
+                //Situação da atividade passa a ser "Em produção"
+                novaAssociacao.IdSituacaoAtividade = 3;
+
+                //Cadastra nova associação no banco de dados
+                ctx.Minhasatividades.Add(novaAssociacao);
+                ctx.SaveChanges();
+            }
+
+           
+        }
+
+        public Atividade BuscarPorId(int id)
+        {
+            return ctx.Atividades.FirstOrDefault(c => c.IdAtividade == id);
+        }
+
+        public void CadastrarAtividade(Atividade atividade)
+        {
+            ctx.Atividades.Add(atividade);
+            ctx.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Método para listar as atividades atribuídas ao usuário
+        /// </summary>
+        /// <param name="id">ID do usuário que terá suas atividades listadas</param>
+        /// <returns>Lista de atividades</returns>
+        public List<MinhasAtividadesViewModel> ListarMinhas(int id)
+        {
+            //Query que busca as atividades associadas ao usuário
+            var listaMinhasAtividade = from atividades in ctx.Atividades
+                                       join minhasAtividades in ctx.Minhasatividades on atividades.IdAtividade equals minhasAtividades.IdAtividade
+                                       where minhasAtividades.IdUsuario == id
+                                       select new MinhasAtividadesViewModel
+                                       {
+                                           IdAtividade = atividades.IdAtividade,
+                                           NomeAtividade = atividades.NomeAtividade,
+                                           DataInicio = atividades.DataInicio,
+                                           DataCriacao = atividades.DataCriacao,
+                                           DataConclusao = atividades.DataConclusao,
+                                           DescricaoAtividade = atividades.DescricaoAtividade,
+                                           RecompensaMoeda = atividades.RecompensaMoeda,
+                                           RecompensaTrofeu = atividades.RecompensaTrofeu,
+                                           NecessarioValidar = atividades.NecessarioValidar,
+                                           IdMinhasAtividades = minhasAtividades.IdMinhasAtividades,
+                                           IdSetor = minhasAtividades.IdSetor,
+                                           IdUsuario = minhasAtividades.IdUsuario,
+                                           IdSituacaoAtividade = minhasAtividades.IdSituacaoAtividade
+                                       };
+                                       
+
+            return listaMinhasAtividade.ToList();
+
+        }
+
+        public List<Atividade> ListarTodas()
+        {
+            return ctx.Atividades.ToList();
+        }
+
+        public void RemoverAtividade(Atividade atividade)
+        {
+            ctx.Atividades.Remove(atividade);
+            ctx.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Método para finalizar uma atividade em curso
+        /// </summary>
+        /// <param name="idUsuario">ID do usuário que terá sua atividade finalizada</param>
+        /// <param name="idAtividade">ID da atividade que será finalizada</param>
+        public void FinalizarAtividade(int idUsuario, int idAtividade)
+        {
+            //Buscando as entidades por meio dos ID's fornecidos
+            Minhasatividade minhaAtividade = ctx.Minhasatividades.FirstOrDefault(a => a.IdAtividade == idAtividade && a.IdUsuario == idUsuario);
+            Atividade atividade = ctx.Atividades.FirstOrDefault(a => a.IdAtividade == idAtividade);
+            Usuario usuario = ctx.Usuarios.FirstOrDefault(u => u.IdUsuario == idUsuario);
+
+            //Verificação se existe a relação entre usuário e atividade
+            if (minhaAtividade != null)
+            {
+                //Verifica se a atividade necessita de validação para ser finalizada
+                if(atividade.NecessarioValidar)
+                {
+                    //Caso necessário, a situação da atividade será alterada para "Aguardando validação"
+                    minhaAtividade.IdSituacaoAtividade = 2;
+                    ctx.Minhasatividades.Update(minhaAtividade);
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    //Caso contrário, a situação da atividade será alterada para "Finalizada" e as recompensas da atividade serão atribuídas ao usuário
+                    minhaAtividade.IdSituacaoAtividade = 1;
+                    usuario.SaldoMoeda = usuario.SaldoMoeda + atividade.RecompensaMoeda;
+                    usuario.Trofeus = usuario.Trofeus + atividade.RecompensaTrofeu;
+                    ctx.Minhasatividades.Update(minhaAtividade);
+                    ctx.Usuarios.Update(usuario);
+                    ctx.SaveChanges();
+                }
+
+            }
+            
+        }
+
+        /// <summary>
+        /// Método para validar uma atividade que está aguardando validação do administrador
+        /// </summary>
+        /// <param name="idAtividade">ID da atividade que será validada</param>
+        /// <param name="idUsuario">ID do usuário que terá sua atividade validada</param>
+        public void ValidarAtividade(int idAtividade, int idUsuario)
+        {
+            //Buscando as entidades por meio dos ID's fornecidos
+            Minhasatividade minhaAtividade = ctx.Minhasatividades.FirstOrDefault(a => a.IdAtividade == idAtividade && a.IdUsuario == idUsuario);
+            Atividade atividade = ctx.Atividades.FirstOrDefault(a => a.IdAtividade == idAtividade);
+            Usuario usuario = ctx.Usuarios.FirstOrDefault(u => u.IdUsuario == idUsuario);
+
+            //Verificação se existe a relação entre usuário e atividade
+            if (minhaAtividade != null)
+            {
+                //Verifica se a situação da atividade é igual à "Aguardando validação"
+                if(minhaAtividade.IdSituacaoAtividade == 2)
+                {
+                    //Caso seja, a atividade terá sua situação trocada para "Finalizada" e as recompensas serão atribuídas ao usuário.
+                    minhaAtividade.IdSituacaoAtividade = 1;
+                    usuario.SaldoMoeda = usuario.SaldoMoeda + atividade.RecompensaMoeda;
+                    usuario.Trofeus = usuario.Trofeus + atividade.RecompensaTrofeu;
+                    ctx.Usuarios.Update(usuario);
+                    ctx.Minhasatividades.Update(minhaAtividade);
+                    ctx.SaveChanges();
+                }
+            }
+        }
+    }
+}
